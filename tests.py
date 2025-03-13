87d2f91ae5a485ff2d68854ee10260e3ccd5151d
Dmitrij Solovyov <dmitriys89@yandex.ru>
2025-03-13 08:31:55 +0300

    Moved the creation of class objects to fixtures

--- tests.py
+++ tests.py
@@ -13,10 +13,7 @@ class TestBooksCollector:
             'Что делать, если ваш кот хочет вас убить'
         ]
     )
-    def test_add_new_book_from_1_to_40_symbols_positive(self, name):
-
-        collector = BooksCollector()
-
+    def test_add_new_book_from_1_to_40_symbols_positive(self, collector, name):
         collector.add_new_book(name)
 
         assert name in collector.books_genre
@@ -28,18 +25,13 @@ class TestBooksCollector:
             'Что делать, если ваш кот хочет вас убитькот хочет вас убить'
         ]
     )
-    def test_add_new_book_over_40_symbols_negative(self, name):
-
-        collector = BooksCollector()
-
+    def test_add_new_book_over_40_symbols_negative(self, collector, name):
         collector.add_new_book(name)
 
         assert name not in collector.get_books_genre()
 
-    def test_add_new_book_re_adding_negative(self):
+    def test_add_new_book_re_adding_negative(self, collector):
         name = 'Гордость и предубеждение'
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
         collector.add_new_book(name)
@@ -47,38 +39,30 @@ class TestBooksCollector:
 
         assert len(collector.get_books_genre()) == 1
 
-    def test_add_new_book_with_empty_line_negative(self):
+    def test_add_new_book_with_empty_line_negative(self, collector):
         name = ''
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
 
         assert name not in collector.books_genre
 
-    def test_set_book_genre_positive(self):
+    def test_set_book_genre_positive(self, collector):
         name = 'Гордость и предубеждение и зомби'
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
         collector.set_book_genre(name, collector.genre[1])
 
         assert collector.get_book_genre(name) == 'Ужасы'
 
-    def test_get_book_genre_without_genre_positive(self):
+    def test_get_book_genre_without_genre_positive(self, collector):
         name = 'Война и мир'
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
 
         assert collector.get_book_genre(name) == ''
 
-    def test_get_book_genre_with_genre_positive(self):
+    def test_get_book_genre_with_genre_positive(self, collector):
         name = 'Гордость и предубеждение и зомби'
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
         collector.set_book_genre(name, collector.genre[1])
@@ -93,19 +77,15 @@ class TestBooksCollector:
             ['Котёнок по имени Гав', 3, 'Котёнок по имени Гав']
         ]
     )
-    def test_get_books_with_specific_genre_positive(self, name, genre, expected_result):
-        collector = BooksCollector()
-
+    def test_get_books_with_specific_genre_positive(self, collector, name, genre, expected_result):
         collector.add_new_book(name)
         collector.set_book_genre(name, collector.genre[genre])
 
         assert collector.get_books_with_specific_genre(collector.genre[genre]) == [expected_result]
 
-    def test_get_books_genre_without_genre_positive(self):
+    def test_get_books_genre_without_genre_positive(self, collector):
         name = 'Гордость и предубеждение и зомби'
         expected_dict = {'Гордость и предубеждение и зомби': ''}
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
 
@@ -119,9 +99,7 @@ class TestBooksCollector:
             ['Котёнок по имени Гав', 3, {'Котёнок по имени Гав': 'Мультфильмы'}]
         ]
     )
-    def test_get_books_genre_positive(self, name, genre, expected_dict):
-        collector = BooksCollector()
-
+    def test_get_books_genre_positive(self, collector, name, genre, expected_dict):
         collector.add_new_book(name)
         collector.set_book_genre(name, collector.genre[genre])
 
@@ -134,47 +112,37 @@ class TestBooksCollector:
             ['Котёнок по имени Гав', 3]
         ]
     )
-    def test_get_books_for_children_positive(self, name, genre):
-
-        collector = BooksCollector()
-
+    def test_get_books_for_children_positive(self, collector, name, genre):
         collector.add_new_book(name)
         collector.set_book_genre(name, collector.genre[genre])
 
         assert name in collector.get_books_for_children()
 
-    def test_get_books_for_children_negative(self):
+    def test_get_books_for_children_negative(self, collector):
         name = 'Гордость и предубеждение и зомби'
-        collector = BooksCollector()
 
         collector.add_new_book(name)
         collector.set_book_genre(name, collector.genre[1])
 
         assert name not in collector.get_books_for_children()
 
-    def test_add_book_in_favorites_positive(self):
+    def test_add_book_in_favorites_positive(self, collector):
         name = 'Человек-амфибия'
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
         collector.add_book_in_favorites(name)
 
         assert name in collector.get_list_of_favorites_books()
 
-    def test_add_book_in_favorites_non_existent_book_negative(self):
+    def test_add_book_in_favorites_non_existent_book_negative(self, collector):
         name = 'Человек-амфибия'
-
-        collector = BooksCollector()
 
         collector.add_book_in_favorites(name)
 
         assert name not in collector.get_list_of_favorites_books()
 
-    def test_add_book_in_favorites_re_adding_negative(self):
+    def test_add_book_in_favorites_re_adding_negative(self, collector):
         name = 'Человек-амфибия'
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
         collector.add_book_in_favorites(name)
@@ -183,10 +151,8 @@ class TestBooksCollector:
 
         assert len(collector.get_list_of_favorites_books()) == 1
 
-    def test_delete_book_from_favorites_positive(self):
+    def test_delete_book_from_favorites_positive(self, collector):
         name = 'Котёнок по имени Гав'
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
         collector.add_book_in_favorites(name)
@@ -194,10 +160,8 @@ class TestBooksCollector:
 
         assert name not in collector.favorites
 
-    def test_delete_book_from_favorites_negative(self):
+    def test_delete_book_from_favorites_negative(self, collector):
         name = 'Котёнок по имени Гав'
-
-        collector = BooksCollector()
 
         collector.add_new_book(name)
         collector.delete_book_from_favorites(name)
@@ -212,10 +176,8 @@ class TestBooksCollector:
             'Котёнок по имени Гав',
         ]
     )
-    def test_get_list_of_favorites_books_positive(self, name):
-
-        collector = BooksCollector()
-
+    def test_get_list_of_favorites_books_positive(self,  collector, name):
         collector.add_new_book(name)
         collector.add_book_in_favorites(name)
+
         assert name in collector.get_list_of_favorites_books()
